2a3cdc6b0574cd4ef35df63bb5514e0aafb66ee6
Aspert <aspert.b@forwardline.com>
2016-07-05 04:16:30 -0700

    Changes by Aspert

---
+++ FLEC2API/src/com/forwardline/api/fundera/FunderaAPIHelper.java
@@ -0,0 +1,14 @@
+package com.forwardline.api.fundera;
+
+public class FunderaAPIHelper {
+
+	public FunderaAPIHelper() {
+		
+	}
+	
+	//TODO 1: Based on the fundera documentation determine what that root type is in the request. The root type must passed as a parameter to this method
+	//TODO 2: Based on the fundera documentation, find the root type of the response and that should be the return type for this method.
+	public void getOffer() {
+		
+	}
+}
---
+++ FLEC2API/src/com/forwardline/salesforce/SalesforceHelper.java
@@ -0,0 +1,12 @@
+package com.forwardline.salesforce;
+
+//TODO 1: This classes should be used for logic around the salesforce rest api
+//TODO 2: Do not cache anything hre
+//TODO 3: Add the login method here. Remember credentials and tokens are passed as parameters.
+public class SalesforceHelper {
+
+	public SalesforceHelper() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
