3172a6976dea6b803b1b9a6b6900ea22fd955d89
shujaath.mohammed <shujaath.mohammed@shujaathm0797.forwardline.local>
2016-07-14 10:10:53 -0700

    Updated for getoffer()

--- FLEC2API/src/com/forwardline/api/fundera/FunderaAPIHelper.java
+++ FLEC2API/src/com/forwardline/api/fundera/FunderaAPIHelper.java
@@ -1,5 +1,6 @@
 package com.forwardline.api.fundera;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.forwardline.api.fundera.pojo.Company;
@@ -8,8 +9,11 @@ import com.forwardline.api.fundera.pojo.FunderaResponse;
 import com.forwardline.api.fundera.pojo.Person;
 import com.forwardline.salesforce.SalesforceFacade;
 import com.forwardline.salesforce.api.LoginResponse;
+import com.forwardline.salesforce.api.pojo.Application;
 import com.forwardline.salesforce.api.pojo.Contact;
+import com.forwardline.salesforce.api.pojo.Customer;
 import com.forwardline.salesforce.api.pojo.Lead;
+import com.forwardline.salesforce.api.pojo.Opportunity;
 
 @SuppressWarnings("unused")
 public class FunderaAPIHelper {
@@ -24,10 +28,17 @@ public class FunderaAPIHelper {
 	}
 
 	private Lead getLead(FunderaRequest request) {
+
 		Company merchant = request.company;
+		Person guarantor = request.getOwners().get(0);
 		Lead l = new Lead();
 		l.setCompanyName(merchant.getBusiness_name());
-		// TODO: set other fields
+		l.setPhone(merchant.getPhone_number());
+		l.setEmail(guarantor.getEmail());
+		l.setFirstName(guarantor.getFirst_name());
+		l.setLastName(guarantor.getLast_name());
+		l.setMobilePhone(guarantor.getPhone_number());
+
 		return l;
 	}
 
@@ -38,8 +49,31 @@ public class FunderaAPIHelper {
 		// are multiple owners.
 		Contact cont = new Contact();
 		cont.setEmail(primaryOwner.getEmail());
-		// TODO: fillin in the other fields
+		cont.setFirstName(primaryOwner.getFirst_name());
+		cont.setLastName(primaryOwner.getLast_name());
+
 		return cont;
+	}
+
+	private Application getApplication(FunderaRequest request) {
+
+		Application app = new Application();
+		Contact primaryContact = getPrimaryContact(request);
+		List<Contact> conList = new ArrayList<Contact>();
+		Lead merchant = getLead(request);
+		Opportunity opp = new Opportunity();
+		opp.setName(request.getCompany().business_name + "_Opp");
+
+		conList.add(primaryContact);
+		app.setGuarantors(conList);
+		app.setLead(merchant);
+		app.setName(request.getCompany().business_name);
+
+		// TODO Set Customer at the time of creation of Application
+		// app.setAccount(account);
+		// app.setOpportunity(opportunity);
+
+		return app;
 	}
 
 	public FunderaResponse getOffers(FunderaRequest request) {
@@ -48,10 +82,74 @@ public class FunderaAPIHelper {
 
 		Contact primaryContact = getPrimaryContact(request);
 		Lead merchant = getLead(request);
+		Application appl = getApplication(request);
+		String partner = "Fundera";
+
+		FunderaAPIHelper helper = new FunderaAPIHelper();
 		try {
 			SalesforceFacade sfFacade = new SalesforceFacade();
 			sfFacade.login(USERNAME, PASSWORD, CLIENTID, SECRETID);
-			
+
+			// Get customer using Primary contact's email address
+			Customer c = sfFacade.getCustomer(primaryContact.getEmail(), partner);
+			Lead existingLead = sfFacade.getLead(merchant.getEmail(), partner);
+
+			// If customer exist, look for application using primary contact's
+			// email
+			if (c != null) {
+				Application app = sfFacade.getApplication(primaryContact.getEmail(), partner);
+
+				// If application exisit, throw error
+				if (app != null) {
+					fndResponse.setSuccess(false);
+				} else {
+
+					// Creating Lead, and then create an Application upon
+					// successful creation of Lead
+
+					Lead l;
+					Application newApp;
+
+					if (existingLead == null)
+						l = sfFacade.createLead(merchant, partner);
+					else
+						l = existingLead;
+
+					if (l != null) {
+						Opportunity opp = new Opportunity();
+						opp.setName(appl.getName() + "-IF");
+						appl.setAccount(c);
+						newApp = sfFacade.createApplication(appl, partner);
+					}
+				}
+			} else {
+
+				// Create Lead, then create a contact, and then create an
+				// application
+				
+				Lead l;
+				if (existingLead == null)
+					l = sfFacade.createLead(merchant, partner);
+				else
+					l = existingLead;
+				
+				Contact con;
+				Application newApp;
+
+				if (l != null) {
+
+					// Create contact
+					con = sfFacade.createContact(primaryContact, partner);
+					if (con != null) {
+
+						// Create application
+						appl.setAccount(c);
+						newApp = sfFacade.createApplication(appl, partner);
+					}
+				}
+
+			}
+
 		} catch (Exception e) {
 			fndResponse.setSuccess(false);
 		}
