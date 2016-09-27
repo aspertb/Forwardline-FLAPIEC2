4e472f486d4cee93381cd9a5c892aa475c3e0551
Aspert <aspert.b@forwardline.com>
2016-09-27 16:15:41 -0700

    Restore

---
+++ FLEC2API/src/com/forwardline/api/fundera/pojo/Company.java
@@ -0,0 +1,244 @@
+package com.forwardline.api.fundera.pojo;
+
+import java.util.Date;
+
+public class Company {
+	public String uuid;
+	public Double loan_amount;
+	public String loan_purpose;
+	public String industry;
+	public String industry_naics;
+	public String business_name;
+	public String business_dba;
+	public String entity_type;
+	public String street_line1;
+	public String street_line2;
+	public String city;
+	public String state;
+	public String zip;
+	public String phone_number;
+	public String ein;
+	public Integer number_of_employees;
+	public Double annual_revenue;
+	public Double average_bank_balance;
+	public Double accounts_receivable;
+	public Date business_inception;
+	public Integer outstanding_tax_lien;
+	public Double credit_card_volume_per_month;
+	public String business_location_type;
+	public Double monthly_business_location_payment;
+	public String business_location_rent_or_own;
+	public String officer_in_lawsuit;
+
+	public Company() {
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public Double getLoan_amount() {
+		return loan_amount;
+	}
+
+	public void setLoan_amount(Double loan_amount) {
+		this.loan_amount = loan_amount;
+	}
+
+	public String getLoan_purpose() {
+		return loan_purpose;
+	}
+
+	public void setLoan_purpose(String loan_purpose) {
+		this.loan_purpose = loan_purpose;
+	}
+
+	public String getIndustry() {
+		return industry;
+	}
+
+	public void setIndustry(String industry) {
+		this.industry = industry;
+	}
+
+	public String getBusiness_name() {
+		return business_name;
+	}
+
+	public void setBusiness_name(String business_name) {
+		this.business_name = business_name;
+	}
+
+	public String getBusiness_dba() {
+		return business_dba;
+	}
+
+	public void setBusiness_dba(String business_dba) {
+		this.business_dba = business_dba;
+	}
+
+	public String getEntity_type() {
+		return entity_type;
+	}
+
+	public void setEntity_type(String entity_type) {
+		this.entity_type = entity_type;
+	}
+
+	public String getStreet_line1() {
+		return street_line1;
+	}
+
+	public void setStreet_line1(String street_line1) {
+		this.street_line1 = street_line1;
+	}
+
+	public String getStreet_line2() {
+		return street_line2;
+	}
+
+	public void setStreet_line2(String street_line2) {
+		this.street_line2 = street_line2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhone_number() {
+		return phone_number;
+	}
+
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+
+	public String getEin() {
+		return ein;
+	}
+
+	public void setEin(String ein) {
+		this.ein = ein;
+	}
+
+	public Integer getNumber_of_employees() {
+		return number_of_employees;
+	}
+
+	public void setNumber_of_employees(Integer number_of_employees) {
+		this.number_of_employees = number_of_employees;
+	}
+
+	public Double getAnnual_revenue() {
+		return annual_revenue;
+	}
+
+	public void setAnnual_revenue(Double annual_revenue) {
+		this.annual_revenue = annual_revenue;
+	}
+
+	public Double getAverage_bank_balance() {
+		return average_bank_balance;
+	}
+
+	public void setAverage_bank_balance(Double average_bank_balance) {
+		this.average_bank_balance = average_bank_balance;
+	}
+
+	public Double getAccounts_receivable() {
+		return accounts_receivable;
+	}
+
+	public void setAccounts_receivable(Double accounts_receivable) {
+		this.accounts_receivable = accounts_receivable;
+	}
+
+	public Date getBusiness_inception() {
+		return business_inception;
+	}
+
+	public void setBusiness_inception(Date business_inception) {
+		this.business_inception = business_inception;
+	}
+
+	public Integer getOutstanding_tax_lien() {
+		return outstanding_tax_lien;
+	}
+
+	public void setOutstanding_tax_lien(Integer outstanding_tax_lien) {
+		this.outstanding_tax_lien = outstanding_tax_lien;
+	}
+
+	public Double getCredit_card_volume_per_month() {
+		return credit_card_volume_per_month;
+	}
+
+	public void setCredit_card_volume_per_month(Double credit_card_volume_per_month) {
+		this.credit_card_volume_per_month = credit_card_volume_per_month;
+	}
+
+	public String getBusiness_location_type() {
+		return business_location_type;
+	}
+
+	public void setBusiness_location_type(String business_location_type) {
+		this.business_location_type = business_location_type;
+	}
+
+	public Double getMonthly_business_location_payment() {
+		return monthly_business_location_payment;
+	}
+
+	public void setMonthly_business_location_payment(Double monthly_business_location_payment) {
+		this.monthly_business_location_payment = monthly_business_location_payment;
+	}
+
+	public String getBusiness_location_rent_or_own() {
+		return business_location_rent_or_own;
+	}
+
+	public void setBusiness_location_rent_or_own(String business_location_rent_or_own) {
+		this.business_location_rent_or_own = business_location_rent_or_own;
+	}
+
+	public String getOfficer_in_lawsuit() {
+		return officer_in_lawsuit;
+	}
+
+	public void setOfficer_in_lawsuit(String officer_in_lawsuit) {
+		this.officer_in_lawsuit = officer_in_lawsuit;
+	}
+
+	public String getIndustry_naics() {
+		return industry_naics;
+	}
+
+	public void setIndustry_naics(String industry_naics) {
+		this.industry_naics = industry_naics;
+	}
+
+}
